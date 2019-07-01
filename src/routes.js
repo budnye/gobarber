f0eb11ec857849d12b0e82780da9f80d0e8dfa9b
Eduardo Budny Pereira <budny@mbp-de-eduardo.localdomain>
2019-07-01 20:56:36 -0300

    Chore: Added users and fixed migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'It works' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Eduardo Budny',
+    email: 'email@email.com',
+    password_hash: '12344123',
+  });
+  return res.json(user);
 });
 
 export default routes;
