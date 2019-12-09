0d4895d50559fccaa7bc5b89bddf21d4ae6389e8
Eduardo Budny Pereira <budny@mbp-de-eduardo.localdomain>
2019-12-09 20:31:26 -0300

    chore: Appointment, notifications and email system

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'fdc6e554d44aef',
+    pass: '26946e67fbdc0e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
